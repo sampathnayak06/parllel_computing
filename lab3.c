4bc5cc82fcd8d8dd8a482d5c7f510a901cb0ff64
Sampath Nayak <sampath.23cs133@sode-edu.in>
2026-08-14 14:43:38 +0530

    Add files via upload

--- lab3.c
+++ lab3.c
@@ -1,50 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#include <math.h>
 
-int is_prime(int num) {
-    if (num <= 1) return 0;
-    if (num == 2) return 1;
-    if (num % 2 == 0) return 0;
-    for (int i = 3; i <= sqrt(num); i += 2) {
-        if (num % i == 0) return 0;
+int fib(int n) {
+    int i, j;
+    if (n < 2)
+        return n;
+    else {
+        #pragma omp task shared(i) firstprivate(n)
+        i = fib(n - 1);
+
+        #pragma omp task shared(j) firstprivate(n)
+        j = fib(n - 2);
+
+        #pragma omp taskwait
+        return i + j;
     }
-    return 1;
 }
 
 int main() {
     int n;
-    printf("Enter the upper limit (n) to find prime numbers: ");
+    printf("Enter the Fibonacci number to calculate: ");
     scanf("%d", &n);
 
-    if (n < 2) {
-        printf("There are no prime numbers up to %d.\n", n);
-        return 0;
+    omp_set_dynamic(0);
+    omp_set_num_threads(4);
+
+    #pragma omp parallel shared(n)
+    {
+        #pragma omp single
+        printf ("fib(%d) = %d\n", n, fib(n));
     }
-
-    printf("\nFinding prime numbers from 1 to %d...\n", n);
-
-    double start_time = omp_get_wtime();
-    int sequential_prime_count = 0;
-    for (int i = 1; i <= n; i++) {
-        if (is_prime(i)) sequential_prime_count++;
-    }
-    double time_seq = omp_get_wtime() - start_time;
-    printf("\nSequential: Found %d primes in %f seconds\n", sequential_prime_count, time_seq);
-
-    start_time = omp_get_wtime();
-    int parallel_prime_count = 0;
-    #pragma omp parallel for reduction(+:parallel_prime_count) schedule(dynamic)
-    for (int i = 1; i <= n; i++) {
-        if (is_prime(i)) parallel_prime_count++;
-    }
-    double time_par = omp_get_wtime() - start_time;
-    printf("Parallel:   Found %d primes in %f seconds\n", parallel_prime_count, time_par);
-
-    if (time_par > 0 && time_seq > 0) {
-        printf("\nSpeedup: %.2fx\n", time_seq / time_par);
-    }
-
     return 0;
 }
